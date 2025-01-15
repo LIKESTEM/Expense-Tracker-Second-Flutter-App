5ebd391a152e382e11fb86f6735d26a8c31794a0
LIKESTEM <thabisotapala@gmail.com>
2025-01-15 12:39:42 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      const MaterialApp(
+        home: Expenses(),
+      ),
+    );
+  });
+}
